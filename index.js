895f031755083f7885ea17c04850d63cb8dd28fc
Leonardo Pessatti <lpessatti@gmail.com>
2022-12-27 10:16:20 -0300

    1.2
    Inclusão node-schedule.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cheerio = require('cheerio');
 const mysql = require('mysql2/promise')
 const {json} = require("express");
 
+const nodeSchedule = require('node-schedule');
+
 const app = express();
 const sql = 'INSERT INTO noticias' +
     '(link, titulo, resumo, imagem)' +
@@ -18,16 +20,15 @@ const bd = {
 
 
 app.get('/', (req, res) =>{
-    const noticias = []
+    const job = nodeSchedule.scheduleJob('*/1 * * * *', () => {
+        const noticias = []
         //Pega a lista de notícias.
         axios.get('https://g1.globo.com/sc/santa-catarina/').then((response) => {
-
             const html = response.data
             const $ = cheerio.load(html)
             const elementos = $('div.medium-uncollapse a, div.large-uncollapse a', html)
-            // Itera pelas notícias.
-            console.log(elementos.length + ' elementos sujos')
 
+            // Itera pelas notícias.
             elementos.each(async function (index) {
 
                 var noticia = []
@@ -38,22 +39,13 @@ app.get('/', (req, res) =>{
                 if ($(this).attr('href') !== undefined) {
                     if ($(this).attr('href').includes('noticia')) {
 
-                        console.log(index + '<>><><><' + $(this).attr('href'))
                         for (let indexx = 0; indexx < elementos.length; indexx++) {
-                            if(indexx != index){
-                                if($(this).attr('href') === $(elementos[indexx]).attr('href')){
-                                        console.log('<<<------>>>')
-                                        // console.log($(this).attr('href'))
-                                        // console.log( $(elemento).attr('href'))
-                                        $(elementos[indexx]).attr('href', 'https://g1.globo.com')
-                                        console.log(indexx + '/' + $(elementos[indexx]).attr('href'))
-                                        console.log('------')
-                                    }
+                            if (indexx != index) {
+                                if ($(this).attr('href') === $(elementos[indexx]).attr('href')) {
+                                    $(elementos[indexx]).attr('href', 'https://g1.globo.com')
                                 }
-
+                            }
                         }
-                        // console.log(elementos.length + ' elementos limpos')
-
 
                         // Pega o link
                         var link = $(this).attr('href')
@@ -72,10 +64,6 @@ app.get('/', (req, res) =>{
                             // Pega a imagem
                             imagem = $n('amp-img', htmlNot).first().attr('src')
                             noticia.push(imagem)
-                            console.log('<' + link)
-                            console.log('<' + titulo)
-                            console.log('<' + resumo)
-                            console.log('<' + imagem)
                             insere(link, titulo, resumo, imagem)
 
                             // Inclui tudo no array final
@@ -89,25 +77,22 @@ app.get('/', (req, res) =>{
 
             async function insere(link, titulo, resumo, imagem) {
                 let connection = await mysql.createConnection(bd)
-                let [rows, fields] =  await connection.execute("SELECT * FROM noticias where link = '" + link + "'")
+                let [rows, fields] = await connection.execute("SELECT * FROM noticias where link = '" + link + "'")
                 connection.end()
                 console.log(rows.length)
                 if (rows.length == 0) {
-                    console.log('nao tem')
+                    console.log('nao tem ' + titulo)
                     let connection = await mysql.createConnection(bd)
-                    console.log('>' + link)
-                    console.log('>' + titulo)
-                    console.log('>' + resumo)
-                    console.log('>' + imagem)
                     let ins = await connection.execute(sql, [link, titulo, resumo, imagem])
                     connection.end()
                     return ins
                 } else {
-                    console.log('ja tem')
+                    console.log('ja tem ' + titulo)
                 }
             }
 
         }).catch((err) => console.log(err))
+    })
     res.json(true)
 })
 
